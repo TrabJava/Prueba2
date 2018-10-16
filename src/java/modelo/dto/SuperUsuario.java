e86517c96e67f8cfc84d47b03573a3beee94041d
TrabJava <42870972+TrabJava@users.noreply.github.com>
2018-10-16 00:07:27 -0300

    SuperUsuario

--- src/java/modelo/dto/SuperUsuario.java
+++ src/java/modelo/dto/SuperUsuario.java
@@ -93,6 +93,13 @@ public class SuperUsuario implements Serializable {
         this.pass = pass;
     }
 
+    public SuperUsuario(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
+    
+    
+
     public Integer getId() {
         return id;
     }
